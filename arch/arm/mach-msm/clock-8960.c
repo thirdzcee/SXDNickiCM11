1cef04c34a7e5248634c40afe7e91c75d4f0c211
NoelMacwan <yulespencer@gmail.com>
2014-04-29 15:29:50 +0530

    fixed typo in overclock/underclock commit

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3552,7 +3552,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_GFX3D(320000000, pll2, 2,  5),
 	F_GFX3D(400000000, pll2, 1,  2),
 	F_GFX3D(450000000, pll15, 1,  2),
-        F_GFX3D(487500000, pll15, 1,  2)
+        F_GFX3D(487500000, pll15, 1,  2),
 	F_END
 };
 
